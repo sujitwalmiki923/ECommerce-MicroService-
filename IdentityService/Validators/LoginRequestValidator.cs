db032e9bd8ecdc4cae9b5a76bccc4b6594663fed
sujit walmiki <sujitwalmiki923@gmail.com>
2026-07-03 03:06:59 +0530

    FluentValidation : Added Fluent validation to set rules for field Using AbstractValidator

---
+++ IdentityService/Validators/LoginRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using IdentityService.DTOs;
+
+namespace IdentityService.Validators
+{
+    public class LoginRequestValidator : AbstractValidator<LoginRequest>
+    {
+        public LoginRequestValidator()
+        {
+                RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress();
+
+            RuleFor(x => x.Password)
+                .NotEmpty();
+        }
+    }
+}
---
+++ IdentityService/Validators/RegisterRequestValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using IdentityService.DTOs;
+
+namespace IdentityService.Validators
+{
+    public class RegisterRequestValidator : AbstractValidator<RegisterRequests>
+    {
+        public RegisterRequestValidator()
+        {
+            //Creates Validation Rules For every Property
+            RuleFor(x => x.FirstName)
+               .NotEmpty()
+               .MaximumLength(50);
+
+            RuleFor(x => x.LastName)
+                .NotEmpty()
+                .MaximumLength(50);
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress();
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .MinimumLength(6)
+                .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
+                .Matches("[a-z]").WithMessage("Password must contain at least one lowercase letter.")
+                .Matches("[0-9]").WithMessage("Password must contain at least one digit.");
+        }
+    }
+}
