d53d7bf8402a9cdd26b36484354c9acd93c60e25
sujit walmiki <sujitwalmiki923@gmail.com>
2026-07-03 17:21:14 +0530

    Included RefreshToken in LoginResponse for returning response in Response Body

--- IdentityService/DTOs/LoginResponse.cs
+++ IdentityService/DTOs/LoginResponse.cs
@@ -2,7 +2,9 @@
 {
     public class LoginResponse
     {
-        public string Token { get; set; } = string.Empty;
+        public string AccessToken { get; set; } = string.Empty;
+
+        public string RefreshToken { get; set; } = string.Empty;
 
         public DateTime ExpiresAt {  get; set; }
     }
