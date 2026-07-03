1b6d3e0d8185cf181cbc77bc756ba1e742180d2f
sujit walmiki <sujitwalmiki923@gmail.com>
2026-07-03 17:00:55 +0530

    Interface and Service Implementation for Refresh Token Table

---
+++ IdentityService/Interfaces/IRefreshTokenRepository.cs
@@ -0,0 +1,13 @@
+﻿using IdentityService.Models;
+
+namespace IdentityService.Interfaces
+{
+    public interface IRefreshTokenRepository
+    {
+        Task AddAsync (RefreshToken refreshToken);
+
+        Task<RefreshToken> GetByTokenAsync(string token);
+
+        Task UpdateAsync(RefreshToken refreshToken);
+    }
+}
---
+++ IdentityService/Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,38 @@
+﻿using IdentityService.Data;
+using IdentityService.Interfaces;
+using IdentityService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityService.Repositories
+{
+    public class RefreshTokenRepository : IRefreshTokenRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RefreshTokenRepository(ApplicationDbContext context)
+        {
+           _context = context;     
+        }
+        public async Task AddAsync(RefreshToken refreshToken)
+        {
+            await _context.RefreshTokens.AddAsync(refreshToken);
+            await _context.SaveChangesAsync();
+            //throw new NotImplementedException();
+        }
+
+        public async Task<RefreshToken?> GetByTokenAsync(string token)
+        {
+            return await _context.RefreshTokens
+                .Include(x => x.User)
+                .FirstOrDefaultAsync(x => x.Token == token);
+           // throw new NotImplementedException();
+        }
+
+        public async Task UpdateAsync(RefreshToken refreshToken)
+        {
+            _context.RefreshTokens.Update(refreshToken);
+            await _context.SaveChangesAsync();
+            //throw new NotImplementedException();
+        }
+    }
+}
