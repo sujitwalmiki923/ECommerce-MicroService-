2d6faea77b8f6732563967d340e9c35f86a644aa
sujit walmiki <sujitwalmiki923@gmail.com>
2026-07-12 17:07:33 +0530

    APIGateway : Added API-Gateway Service using YARP

---
+++ src/ApiGateway/Program.cs
@@ -0,0 +1,17 @@
+using Yarp.ReverseProxy;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddReverseProxy()
+    .LoadFromConfig(
+      builder.Configuration.GetSection("ReverseProxy")
+    );
+
+var app = builder.Build();
+
+app.MapReverseProxy();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
